19bf0a738202cb6ffbcb318d4919812123f23b06
Vikram Saketh Garre <vikramsakethg@Vikrams-MacBook-Pro.local>
2026-04-23 16:21:06 +0530

    test auto deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,22 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "devops_server" {
-  ami           = "ami-0f5ee92e2d63afc18"   # Amazon Linux (update if needed)
+  ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
+  key_name      = "landslide-key"   # 👈 ADD THIS LINE HERE
 
   tags = {
-    Name = "DevOps-Server"
+    Name = "Landslide-DevOps"
   }
+}
+
+resource "aws_eip" "devops_ip" {
+  instance = aws_instance.devops_server.id
+  domain   = "vpc"
+}
+
+output "public_ip" {
+  value = aws_eip.devops_ip.public_ip
 }
